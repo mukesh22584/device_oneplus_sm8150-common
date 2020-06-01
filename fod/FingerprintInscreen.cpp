47019c2e7fe74e548c4ac12d39d92a656278fad9
Marko Man <darkobas@gmail.com>
2020-06-01 10:56:03 +0530

    sm8150-common: fod: Always set OP_DISPLAY_SET_DIM=1 on fp press
    * That way we no longer have to turn HBM for
      the whole fp enroll process.

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -21,7 +21,6 @@
 #include <hidl/HidlTransportSupport.h>
 #include <fstream>
 
-#define FINGERPRINT_ERROR_CANCELED 5
 #define FINGERPRINT_ACQUIRED_VENDOR 6
 #define FINGERPRINT_ERROR_VENDOR 8
 
@@ -80,6 +79,7 @@ Return<void> FingerprintInscreen::onFinishEnroll() {
 }
 
 Return<void> FingerprintInscreen::onPress() {
+    this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
     this->mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 1);
 
     return Void();
